2fde2e58c86e7b0c3c18374a3fe8a12c6299b29f
herrylegowo <herrylegowo19@gmail.com>
2020-05-10 22:17:00 +0800

    debuging route edit

--- routes/article.js
+++ routes/article.js
@@ -34,7 +34,7 @@ router.delete('/:id', async(req, res) => {
 function saveArticleAndRedirect(path){
     return async (req, res) =>{
         let article = req.article
-            article.title = req.body.title,
+            article.title = req.body.title
             article.description = req.body.description
             article.markdown = req.body.markdown
         try {
